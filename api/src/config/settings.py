afeed75dc38303cb711b94c9c5badeb1e330578f
Fernando Toledo <fernandohtr@gmail.com>
2024-04-06 14:31:02 -0300

    build: add django-cors-headers dependencies

--- api/src/config/settings.py
+++ api/src/config/settings.py
@@ -14,6 +14,7 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "corsheaders",
 ]
 LOCAL_APPS = [
     "v1",
@@ -22,6 +23,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -76,3 +78,4 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ]
 }
+CORS_ORIGIN_ALLOW_ALL = True
