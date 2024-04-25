7c9d01b50e2fa97f85c808a0e52b9365e4fa93b0
Fernando Toledo <fernandohtr@gmail.com>
2024-04-24 21:09:09 -0300

    feat: refact sponsors and add favicon

--- web/src/components/Sponsor.tsx
+++ web/src/components/Sponsor.tsx
@@ -1,20 +1,42 @@
 import "./Sponsor.css"
 
+import { useEffect, useState } from "react"
+
+interface Sponsor {
+  name: string
+  url_social_media: string
+  image: string
+}
+
+
 export function Sponsor() {
+  const [ sponsors, setSponsors ] = useState<Sponsor[]>([])
+
+  useEffect(() => {
+    const apiUrl = new URL("http://localhost:8001/api/v1/sponsors")
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        setSponsors(data)
+      })
+      .catch(error => console.warn(error))
+  }, [])
+
   return (
     <section className="sponsors">
-      <h2>Sponsors</h2>
-      <ul>
-        <div className="sponsor-card">
-          <li>patrocinador</li>
-        </div>
-        <div className="sponsor-card">
-          <li>patrocinador</li>
-        </div>
-        <div className="sponsor-card">
-          <li>patrocinador</li>
-        </div>
-      </ul>
+      <h2>Patrocinadores</h2>
+      <div className="sponsors-container">
+        { sponsors.map(sponsor => {
+          return (
+            <div className="sponsor-card" key={ sponsor.name }>
+              <a href={ sponsor.url_social_media } target="_blank">
+                <img src={ sponsor.image } />
+              </a>
+            </div>
+          )
+        })}
+      </div>
     </section>
   )
 }
