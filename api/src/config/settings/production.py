1e8fcda57ebf9efa974fc0d572747a6fc963b835
Fernando Toledo <fernandohtr@gmail.com>
2024-05-09 14:42:39 -0300

    feat: set deploy configurations

--- api/src/config/settings/production.py
+++ api/src/config/settings/production.py
@@ -5,6 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     ".localhost",
     ".127.0.0.1",
+    ".fernandohtr.com",
 ]
 DATABASES = {
     "default": {
@@ -21,4 +22,11 @@ MEDIA_ROOT = "/mediafiles/"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://cardapio.fernandohtr.com",
 ]
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
