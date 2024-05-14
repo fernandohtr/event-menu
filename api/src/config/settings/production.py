d1e6971d5c8465544e14a02166dc92dcd62eecf4
Fernando Toledo <fernandohtr@gmail.com>
2024-05-14 17:31:40 -0300

    refactor: adjust production settings

--- api/src/config/settings/production.py
+++ api/src/config/settings/production.py
@@ -1,11 +1,16 @@
 from .base import *
 
+ADMINS = [("Fernando Toledo", "fernandohtr@gmail.com")]
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = False
 ALLOWED_HOSTS = [
     ".localhost",
     ".127.0.0.1",
     ".fernandohtr.com",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://cardapio.fernandohtr.com",
+    "http://cardapio.fernandohtr.com",
 ]
 DATABASES = {
     "default": {
@@ -23,10 +28,15 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
     "https://cardapio.fernandohtr.com",
+    "http://cardapio.fernandohtr.com",
 ]
-SECURE_HSTS_SECONDS = 3600
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+DJANGO_SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 60 * 60 * 24 * 6  # 518400
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 CSRF_COOKIE_SECURE = True
